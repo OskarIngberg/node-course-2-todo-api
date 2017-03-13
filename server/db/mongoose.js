f193e3afa3cf0749cae6d974d104a6c23fffe834
Oskar Ingberg <oskar.ingberg@ninetech.com>
2017-03-13 09:37:27 +0100

    db/mongoose.js added to gitignore

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://test:test@ds127300.mlab.com:27300/node-todo-api');
-mongoose.connect('mongodb://test:test@ds127300.mlab.com:27300/node-todo-api');
+mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://test:test@ds127300.mlab.com:27300/node-todo-api');
+// mongoose.connect('mongodb://test:test@ds127300.mlab.com:27300/node-todo-api');
 
 
 module.exports = {mongoose};
