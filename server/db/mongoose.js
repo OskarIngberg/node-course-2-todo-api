bf9b04b2fa4a641f75a41073c855444e9413b20f
Oskar Ingberg <oskar.ingberg@ninetech.com>
2017-03-13 10:25:08 +0100

    Setup seperate test and delopment envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://test:test@ds127300.mlab.com:27300/node-todo-api');
+mongoose.connect(process.env.MONGODB_URI);
 // mongoose.connect('mongodb://test:test@ds127300.mlab.com:27300/node-todo-api');
 
 
