8c684f35b8e1beac93e29cdb0dd5b34a6133ae7e
Oskar Ingberg <oskar.ingberg@ninetech.com>
2017-03-14 09:58:46 +0100

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,24 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else if (env === 'production'){
-    process.env.MONGODB_URI = 'mongodb://test:test@ds127300.mlab.com:27300/node-todo-api';
+if(env === 'development' || env === 'test'){
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) =>{
+        process.env[key] = envConfig[key];
+    });
 }
+
+// if(env === 'development'){
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test'){
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// } else if (env === 'production'){
+//     process.env.MONGODB_URI = 'mongodb://test:test@ds127300.mlab.com:27300/node-todo-api';
+// }
+
+// if (env === 'production'){
+//     process.env.MONGODB_URI = 'mongodb://test:test@ds127300.mlab.com:27300/node-todo-api';
+// }
